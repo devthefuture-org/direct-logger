e814a40d937a91c3cc5c43715607bb820e87c531
devthejo <jo@surikat.pro>
2023-03-02 15:37:37 +0100

    fix: clean

--- write-stream.js
+++ write-stream.js
@@ -1,22 +1,18 @@
 const { Writable } = require('node:stream');
 
-const nullFunc = ()=>{}
-
 module.exports = class WritableStream extends Writable {
   constructor(config={}){
     super()
-    const {logger, level="info", raw=false, splitLines=true} = config
+    const {logger, level="info", raw=false} = config
     this.logger = logger
     this.level = level
     this.raw = raw
-    this.splitLines = splitLines
   }
   _write(data, enc, cb){
     const {
       logger,
       level,
       raw,
-      splitLines,
     } = this
     if(raw) {
       const i = logger.levels.indexOf(level)
@@ -25,12 +21,6 @@ module.exports = class WritableStream extends Writable {
         return
       }
       return stream.write(data, enc, cb)
-    } else if(splitLines) {
-      const lines = data.toString().split("\n")
-      lines.map(line=>{
-        logger[level](line, {}, nullFunc)
-      })
-      cb()
     } else {
       logger[level](data, {}, cb)
     }
